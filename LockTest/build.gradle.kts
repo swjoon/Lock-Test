62adfa9a3df6236e8456ad9305d7c9456f2d5f93
swjoon <jetkid1446@gmail.com>
2025-05-26 10:29:51 +0900

    initial setting

---
+++ LockTest/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.4"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.backend"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// redis & redisson
+	implementation("org.redisson:redisson-spring-boot-starter:3.47.0")
+
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	testImplementation("org.testcontainers:mysql")
+	testImplementation("org.testcontainers:redis")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ LockTest/src/main/java/com/backend/LockTest/LockTestApplication.java
@@ -0,0 +1,12 @@
+package com.backend.LockTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockTestApplication.class, args);
+	}
+}
